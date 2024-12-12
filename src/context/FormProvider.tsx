fcbb7745016f772c449236c0b9884c48382a124e
Crystal D <crystal12duran12@outlook.es>
2024-12-12 01:37:56 -0400

    feat: add in the FormProvider all the properties to the state of FormData and create a function to know if there is content in it

--- src/context/FormProvider.tsx
+++ src/context/FormProvider.tsx
@@ -1,5 +1,5 @@
-import { useState, ReactNode  } from "react";
-import { FormData } from "../interfaces/interfaces";
+import { useState, ReactNode } from "react";
+import { FormData } from "../interfaces/form";
 import { FormContext } from "./FormContext";
 
 interface FormProviderProps {
@@ -12,15 +12,48 @@ export const FormProvider = ({ children }: FormProviderProps) => {
         mantra: '',
         imagesHome: [],
         career: '',
-        imagesCareer: []
+        imagesCareer: [],
+        lifestyle: [],
+        imagesOutfit: [],
+        affirmations: [],
+        imagesExperiences: [],
+        imageCar: [],
+        imagesTech: [],
+        imagesHealth: [],
+        imagesCustom: [],
+        stickers: []
     });
 
     const updateFormData = (newData: Partial<FormData>) => {
         setFormData((prevData) => ({ ...prevData, ...newData }));
     };
 
+    // utility function to check if context has something
+    const hasContextContent = () => {
+        // check string fields
+        const stringFields: (keyof FormData)[] = [
+            'name', 'mantra', 'career'
+        ];
+        const hasNonEmptyStrings = stringFields.some(
+            field => formData[field] && formData[field] !== ''
+        );
+
+        // check array fields
+        const arrayFields: (keyof FormData)[] = [
+            'imagesHome', 'imagesCareer', 'lifestyle',
+            'imagesOutfit', 'affirmations', 'imagesExperiences',
+            'imageCar', 'imagesTech', 'imagesHealth',
+            'imagesCustom', 'stickers'
+        ];
+        const hasNonEmptyArrays = arrayFields.some(
+            field => formData[field] && formData[field].length > 0
+        );
+
+        return hasNonEmptyStrings || hasNonEmptyArrays;
+    };
+
     return (
-        <FormContext.Provider value={{ formData, updateFormData }}>
+        <FormContext.Provider value={{ formData, updateFormData, hasContextContent}}>
             {children}
         </FormContext.Provider>
     );
