0edb63fe2e050e1998668bee9eb15bdb4ecf4ba7
Crystal D <crystal12duran12@outlook.es>
2024-12-12 02:19:39 -0400

    feat: add result route with result component within protected route

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css';
 import { FormProvider } from './context/FormProvider';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import Home from './pages/home/Home';
 import { Form } from './pages/form/Form';
+import { Result } from './pages/result/Result';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FormProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="create" element={<Form />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="result" element={<ProtectedRoute><Result/></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
     </FormProvider>
   </StrictMode>,
 )
